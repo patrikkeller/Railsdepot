d5c51e19fee5629838792d1af853402862957120
patrikkeller <patrik.keller@gmail.com>
2011-05-24 14:23:19 +0200

    11.1 cart in sidebar mit partials

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -63,7 +63,8 @@ class LineItemsController < ApplicationController
 	respond_to do |format|
 		if @line_item.save
 			# format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.') }
-			format.html { redirect_to(@line_item.cart) }
+			# format.html { redirect_to(@line_item.cart) } # zeigt cart nach adden an
+			format.html { redirect_to(store_url) }  # zeigt den Cart in der Sidebar an
 			format.xml { render :xml => @line_item, :status => :created, :location => @line_item }
 		else
 			format.html { render :action => "new" }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
   def index
 	@products = Product.all
+	
+	@cart = current_cart
   end
 
 end
